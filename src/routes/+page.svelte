5e23ef1cf2da2f3a21bc5ec061436780daaded1c
AphoticArk <mshields.ark@gmail.com>
2024-03-26 17:20:19 -0400

    made some style and function changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,39 @@
 <script>
      import '../style.css';
+     import {writable} from 'svelte/store';
      let todoItem = '';
-     let todoList = [];
+     let storedList;
+     let todoList = writable([]);
+
+     if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
+          storedList = localStorage.getItem('storedList');
+          if (storedList) {
+               $todoList = (JSON.parse(storedList));
+          }
+     }
+
+     function updateList() {
+          return storedList = localStorage.setItem('storedList', JSON.stringify($todoList));
+     }
+
+     $: isDone = $todoList.filter(item => item.done);
 
      function addToArray() {
           if (todoItem == '') {
                return;
           }
-          todoList = [...todoList,{
+          $todoList = [...$todoList,{
                text: todoItem,
                done: false
           }];
-          console.log(todoList);
+          // console.log(todoList);
+          updateList();
           todoItem = '';
      }
      function removeThis(index) {
-          todoList.splice(index, 1)
-          todoList = todoList;
+          $todoList.splice(index, 1)
+          $todoList = $todoList;
+          updateList();
      }
 </script>
 
@@ -28,21 +45,35 @@
 </form>
 
 <ul>
-     {#each todoList as item, index}
+     {#each $todoList as item, index}
           <li>
-               <input type="checkbox" bind:checked={item.done}>
+               <input type="checkbox" bind:checked={item.done} on:click={() => setTimeout(() => removeThis(index), 3000)} >
+
                <span class:done={item.done}>{item.text}</span>
                <span on:click={() => removeThis(index)} class="remove" role='button' tabindex='0'>&times;</span>
           </li>
      {/each}
 </ul>
-
+<!-- {#if isDone.length > 0} -->
 <style> 
+     h1, form, li{
+          display: flex;
+          justify-content: center;
+     }
+
+     form {
+          margin-top: 3vw;
+          margin-bottom: 4vw;
+     }
      ul {
           list-style: none;
      }
      li {
           font-size: 1.3rem;
+          margin-right: 3vw;
+     }
+     span {
+          margin: 0 .3vw;
      }
      .done {
           color: #ccc;
