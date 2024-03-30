5b70c405fb94eb3350d3b555a291e6454760c483
AphoticArk <mshields.ark@gmail.com>
2024-03-29 23:09:01 -0400

    Finished site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,21 +2,34 @@
      import '../style.css';
      import {writable} from 'svelte/store';
      let todoItem = '';
-     let storedList;
+     let storedListNotDone;
+     let storedListDone;
      let todoList = writable([]);
+     let finishedList = writable([]);
+     let temp = '';
 
      if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
-          storedList = localStorage.getItem('storedList');
-          if (storedList) {
-               $todoList = (JSON.parse(storedList));
+          storedListNotDone = localStorage.getItem('storedListNotDone');
+          if (storedListNotDone) {
+               $todoList = (JSON.parse(storedListNotDone));
           }
      }
 
-     function updateList() {
-          return storedList = localStorage.setItem('storedList', JSON.stringify($todoList));
+     if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
+          storedListDone = localStorage.getItem('storedListDone');
+          if (storedListDone) {
+               $finishedList = (JSON.parse(storedListDone));
+          }
      }
 
-     $: isDone = $todoList.filter(item => item.done);
+     function updateTaskList() {
+          return storedListNotDone = localStorage.setItem('storedListNotDone', JSON.stringify($todoList));
+     }
+     function updateDoneList() {
+          return storedListDone = localStorage.setItem('storedListDone', JSON.stringify($finishedList));
+     }
+
+     $: isDone = $finishedList.filter(item => item.done);
 
      function addToArray() {
           if (todoItem == '') {
@@ -27,12 +40,51 @@
                done: false
           }];
           // console.log(todoList);
-          updateList();
+          updateTaskList();
+          updateDoneList();
           todoItem = '';
      }
-     function removeThis(index) {
-          $todoList.splice(index, 1)
+     function removeThisTask(index) {
+
+          $todoList.splice(index, 1);
           $todoList = $todoList;
+          updateTaskList();
+          updateDoneList();
+          
+     }
+     function removeThisDone(index) {
+
+          $finishedList.splice(index, 1);
+          $finishedList = $finishedList;
+          updateTaskList();
+          updateDoneList();
+          
+     }
+
+     function pushThis(item, index) {
+
+          temp = item;
+          $todoList.splice(index, 1);
+          $todoList = $todoList;
+          $finishedList.push(temp);
+          $finishedList = $finishedList;
+          updateTaskList();
+          updateDoneList();
+          console.log(temp);
+     }
+     function pushBack(item, index) {
+
+          temp = item;
+          $finishedList.splice(index, 1);
+          $finishedList = $finishedList;
+          $todoList.push(temp);
+          $todoList = $todoList;
+          updateTaskList();
+          updateDoneList();
+          console.log(temp);
+     }
+     function clearDone() {
+          $finishedList = $finishedList.filter(item => !item.done)
           updateList();
      }
 </script>
@@ -44,26 +96,78 @@
      <button type="submit">Add</button>
 </form>
 
-<ul>
+<ul class="notDone">
      {#each $todoList as item, index}
+          <img class="divide" src="https://riyogames.com/img/ui/rule_of_law.png" alt="">
           <li>
-               <input type="checkbox" bind:checked={item.done} on:click={() => setTimeout(() => removeThis(index), 3000)} >
+               <input type="checkbox" bind:checked={item.done} on:click={() => setTimeout(() => pushThis(item, index), 1000)} >
 
                <span class:done={item.done}>{item.text}</span>
-               <span on:click={() => removeThis(index)} class="remove" role='button' tabindex='0'>&times;</span>
+               <span on:click={() => removeThisTask(index)} class="remove" role='button' tabindex='0'>&times;</span>
           </li>
      {/each}
 </ul>
+
+{#if isDone.length > 0}
+     <h2>Completed</h2>
+{/if}
+
+<ul class="finishedList">
+     {#each $finishedList as item, index}
+          <img class="divide" src="https://riyogames.com/img/ui/rule_of_law.png" alt="">
+          <li>
+               <!-- <input type="checkbox" checked='false' on:click={() => setTimeout(() => pushBack(item, index), 1000)}> -->
+
+               <span class:done={item.done}>{item.text}</span>
+               <span on:click={() => removeThisDone(index)} class="remove" role='button' tabindex='0'>&times;</span>
+          </li>
+     {/each}
+</ul>
+
+{#if isDone.length > 0}
+     <button class='clear' on:click={clearDone}>Remove Done</button>
+{/if}
+
 <!-- {#if isDone.length > 0} -->
 <style> 
-     h1, form, li{
+
+     h1, h2, form, li, span{
           display: flex;
           justify-content: center;
+          color: darkslateblue;
      }
-
+     .clear {
+          display: flex;
+          justify-content: center !important;
+          margin: auto;
+     }
+     ul img {
+          display: flex;
+          justify-content: center !important;
+     }
+     h1 {
+          font-weight: 700;
+          color: midnightblue;
+     }
+     h2 {
+          margin-top: 4vw;
+          color: midnightblue;
+     }
      form {
           margin-top: 3vw;
           margin-bottom: 4vw;
+          font-weight: 400;
+     }
+     input {
+          margin-left: 1.5em;
+          margin-right: .7em;
+          background-color: skyblue;
+          color: darkslateblue;
+     }
+     button {
+          background-color: royalblue;
+          color:darkblue;
+          border-radius: 6px;
      }
      ul {
           list-style: none;
@@ -71,17 +175,27 @@
      li {
           font-size: 1.3rem;
           margin-right: 3vw;
+          font-weight: 400;
      }
      span {
           margin: 0 .3vw;
+
      }
-     .done {
-          color: #ccc;
-          text-decoration: line-through;
+     .divide {
+          height: 100%;
+          width: 30%;
+          margin: .5em auto;
      }
      .remove {
           color: #c00;
           cursor: pointer;
+     }
+     .notDone li {
+          color: #000;
+          text-decoration: none;
+     }
+     .finishedList {
+          color: #aaa;
      }
 
 </style>
